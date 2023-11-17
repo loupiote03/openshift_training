7daa1e316a2c3e44dda942e038768cb71938c921
bastien larrouture <bastien.larrouture@cgi.com>
2023-11-17 15:14:14 +0100

    n

--- testDockerPostgres/user.sql
+++
@@ -1,8 +0,0 @@
-create table accounts(
-    user_id serial PRIMARY KEY,
-    username varchar(50) UNIQUE NOT NULL,
-    pwd varchar(50) NOT NULL,
-    email varchar(50) UNIQUE NOT NULL
-);
-
-insert into accounts(username, pwd, email) values ('JDoe', '123456789', 'john.doe@mail.com');
